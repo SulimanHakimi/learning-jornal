5461499a44924663655bbfd0066727e3be8badc8
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-02-25 20:00:54 -0800

    fit width responsev for arti

--- src/components/recentPost.jsx
+++ src/components/recentPost.jsx
@@ -14,7 +14,7 @@ function RecentPost() {
     <>
       <Nav />
       <div className="sm:px-6 lg:px-20 h-fit py-20 gap-10 flex flex-col justify-center items-center">
-        <div className="px-28 flex flex-col gap-4">
+        <div className="lg:px-28 sm:px-5 md:px-16 flex flex-col gap-4">
           <span>{articale.date}</span>
           <h1 className="font-extrabold sm:text-xl md:text-2xl lg:text-3xl">
             {articale.title}
