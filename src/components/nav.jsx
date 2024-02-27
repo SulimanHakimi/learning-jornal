f57c11df64d16ea5eb7fbda9e4ffb61fd9db33e8
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-02-26 20:02:11 -0800

    add for samller scren fgbk

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -19,11 +19,11 @@ function Nav() {
             src="https://s3-alpha-sig.figma.com/img/0d3c/e4ac/61b804f61e5f03da50af5674102e33e3?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=my3MtdSEHE2oTCNfqFtJli6h6WRHMwXdYhK5a6cqnOGEeWDqi4HwUI2rnwTKvPIv3cZrpZ~58R-eqyAZHmLbshBCyR7tv3~XvrOtfixjktZuwFarD~v0Ld8d02s~pDm4jE5TaOsS7Rzsee-0sebs3rTnzAt~2AwMzPZ38ZyOdimCXx1RVgldbmxqIbGVgiBUczpbHdCwe8VNt3ByqOdoL1n~oS7oRX~KoFH9~npWZ4kLenhn~hekHUdwhOMCVS946hZBWUccu44S~-lR096HgL4SciDo5dOQ62EMhDTmnn8RIlE1hWbqSfkj1wes2fDMt~soxrhw6WLnm7YJWB7DTA__"
             alt=""
           />
-          <h1 className="sm:text-base md:text-lg sm:font-medium lg:text-xl  lg:font-bold">
+          <h1 className="sm:text-sm md:text-lg sm:font-medium lg:text-xl  lg:font-bold">
             My learning journal
           </h1>
         </div>
-        <ul className="flex md:gap-7 lg:gap-10 items-center font-medium sm:gap-2">
+        <ul className="flex md:gap-7 lg:gap-10 items-center sm:text-sm font-normal sm:gap-3">
           <li className="cursor-pointer">
             <Link to={"/"}>HOME</Link>
           </li>
@@ -33,9 +33,9 @@ function Nav() {
           <li className="cursor-pointer">
             <button onClick={setThemeChange}>
               {theme === "dark" ? (
-                <CiLight size={30} />
+                <CiLight size={25} />
               ) : (
-                <MdOutlineDarkMode size={30} />
+                <MdOutlineDarkMode size={25} />
               )}
             </button>
           </li>
